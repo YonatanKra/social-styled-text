5a7f3856dbde609e43c151bd8b37c4c4f2fc9cb1
yonatankra <yonatan.kra@vonage.com>
2022-08-28 11:37:51 +0300

    Make sure to trigger on the current tab

--- ui/popup.spec.js
+++ ui/popup.spec.js
@@ -1,3 +1,6 @@
+import chrome from 'sinon-chrome';
+global.chrome = chrome;
+
 function setButtonInPage() {
     document.body.innerHTML = '<button class="bolderize">Bolderize</button>';
 }
@@ -12,6 +15,9 @@ function spyOnClickCallback(bolderizeButton) {
 }
 
 describe(`popup`, function () {
+    afterEach(function () {
+        jest.restoreAllMocks();
+    });
     it(`should set a listener on the bolderize button`, async function () {
         setButtonInPage();
         const bolderizeButton = document.querySelector('.bolderize');
@@ -19,7 +25,19 @@ describe(`popup`, function () {
         await import('./popup.js');
 
         bolderizeButton.click();
-        
+
         expect(spy).toHaveBeenCalled();
+    });
+
+    it(`should work on current tab`, async function () {
+        jest.spyOn(chrome.tabs, 'query');
+        
+        setButtonInPage();
+        const bolderizeButton = document.querySelector('.bolderize');
+        await import('./popup.js');
+
+        bolderizeButton.click();
+
+        expect(chrome.tabs.query).toHaveBeenCalledWith({active: true, currentWindow: true}, expect.any(Function));
     });
 });
