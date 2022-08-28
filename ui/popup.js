c7725b18ed63f0aa83d4f9a7b5974a69ee4fdd01
yonatankra <yonatan.kra@vonage.com>
2022-08-28 14:12:35 +0300

    Change implementation - tests still pass!

--- ui/popup.js
+++ ui/popup.js
@@ -1,13 +1,17 @@
-import {bolderizeWord} from "../utils/bolderizeWord";
+import {bolderizeWord} from "../utils/bolderizeWord.js";
 
 function getSelectionText() {
     return window.getSelection().toString();
 }
 
 function modifySelection(newValue) {
-    const range = document.getSelection().getRangeAt(0);
-    range.deleteContents();
-    range.insertNode(document.createTextNode(newValue));
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    const clone = range.cloneRange();
+
+    range.endContainer.textContent = range.endContainer.textContent.replace(range.cloneContents().textContent, newValue);
+
+    selection.addRange(clone);
 }
 
 const button = document.querySelector('.bolderize');
