1f43abcb08949e5209f4d8f6ffc289b4ab34795e
yonatankra <yonatan.kra@vonage.com>
2022-08-28 11:28:07 +0300

    Refactor test

--- ui/popup.spec.js
+++ ui/popup.spec.js
@@ -1,14 +1,25 @@
+function setButtonInPage() {
+    document.body.innerHTML = '<button class="bolderize">Bolderize</button>';
+}
+
+function spyOnClickCallback(bolderizeButton) {
+    const originalAddEventListener = bolderizeButton.addEventListener;
+    const spy = jest.fn();
+    jest.spyOn(bolderizeButton, 'addEventListener').mockImplementation((eventName, _) => {
+        originalAddEventListener(eventName, spy);
+    });
+    return spy;
+}
+
 describe(`popup`, function () {
     it(`should set a listener on the bolderize button`, async function () {
-        document.body.innerHTML = '<button class="bolderize">Bolderize</button>';
+        setButtonInPage();
         const bolderizeButton = document.querySelector('.bolderize');
-        const originalAddEventListener = bolderizeButton.addEventListener;
-        const spy = jest.fn();
-        jest.spyOn(bolderizeButton, 'addEventListener').mockImplementation((eventName, _) => {
-            originalAddEventListener(eventName, spy);
-        });
+        const spy = spyOnClickCallback(bolderizeButton);
         await import('./popup.js');
+
         bolderizeButton.click();
+        
         expect(spy).toHaveBeenCalled();
     });
 });
